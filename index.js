7c4f10e4895ee334d5c40406eec2655151121fdc
Corbin Page <corbin.page@gmail.com>
2016-07-29 16:28:42 -0400

    Saving off unique transactions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var bluebird = require('bluebird');
 var plaid = require('plaid');
 var fs = require('fs');
+var low = require('lowdb');
+var _ = require('lodash');
 
 // Promisify the plaid module
 bluebird.promisifyAll(plaid);
@@ -13,13 +15,61 @@ var client = new plaid.Client(
 
 var addAuthUserAsync = bluebird.promisify(client.addAuthUser, {context: client, multiArgs: true});
 var stepAuthUserAsync = bluebird.promisify(client.stepAuthUser, {context: client, multiArgs: true});
+
 var addConnectUserAsync = bluebird.promisify(client.addConnectUser, {context: client, multiArgs: true});
 var stepConnectUserAsync = bluebird.promisify(client.stepConnectUser, {context: client, multiArgs: true});
 var getConnectUserAsync = bluebird.promisify(client.getConnectUser, {context: client, multiArgs: true});
 
+// ---------------------------------------------------------------
+const db = low('tmp/db.json');
+// db._.mixin(require('underscore-db'));
+// const db = low('db.json', { storage: require('lowdb/lib/file-async') })
+
+db.defaults({ transactions: [] }).value();
+
+// db._.mixin({
+//   createTransactions: function(array) {
+//     console.log(array);
+//     array.forEach( t => {
+//       var transaction = _(db.transactions)
+//       .where({_id: t._id})
+//       .value()
+
+//       console.log(transaction);
+
+//       if(_.isEmpty(transaction)) {
+//         db.get('transactions')
+//         .push(t)
+//         .value()
+//       }
+//     })
+//   }
+// })
+
 
 // ---------------------------------------------------------------
 
+var access_token = '8c1a3ea14f768c659c682b6da5cbed143152316a78357270de6afeab111a2c2b66975918488140d1af39da8c18f0937fee101b5df3b7a4bbbeb3ada5754e28af544ddcec61d862b4f39892ed60a1f132';
+
+getConnectUserAsync(access_token).then(responses => {
+  var response = responses[0];
+
+  response.transactions.forEach(t => {
+    var isNewTransaction = _.isEmpty(db.get('transactions')
+      .find({ _id: t._id })
+      .value());
+
+    if(isNewTransaction) {
+      db.get('transactions')
+      .push(t)
+      .value()         
+    }
+
+  })
+
+});
+
+// ---------------------------------------------------------------
 
 // addConnectUserAsync('chase', {
 //   username: '',
@@ -31,15 +81,22 @@ var getConnectUserAsync = bluebird.promisify(client.getConnectUser, {context: cl
 //   console.log(response);
 
 
-//   fs.writeFile("tmp/text.json", JSON.stringify(response), function(err) {
+//   fs.writeFile("tmp/test.json", JSON.stringify(response), function(err) {
 //     if(err) {
 //       return console.log(err);
 //     }
+
+//     db.get('posts')
+//     .push(response.transactions)
+//     .value()
 
 //     console.log("The file was saved!");
 //   }); 
 
 // });
+
+
+
 
 
 // ---------------------------------------------------------------
@@ -70,26 +127,6 @@ var getConnectUserAsync = bluebird.promisify(client.getConnectUser, {context: cl
 
 
 // ---------------------------------------------------------------
-
-
-var access_token = '8c1a3ea14f768c659c682b6da5cbed143152316a78357270de6afeab111a2c2b66975918488140d1af39da8c18f0937fee101b5df3b7a4bbbeb3ada5754e28af544ddcec61d862b4f39892ed60a1f132';
-
-getConnectUserAsync(access_token).then(responses => {
-  var mfaResponse = responses[0];
-  var response = responses[1];
-  console.log(mfaResponse);
-  console.log(response);
-
-
-  fs.writeFile("tmp/text.json", JSON.stringify(response), function(err) {
-    if(err) {
-      return console.log(err);
-    }
-
-    console.log("The file was saved!");
-  }); 
-
-});
 
 
 
