609b1d3f3eab675fe74a6fa6e10f5c803eb3d2d9
Corbin Page <corbin.page@gmail.com>
2016-08-23 23:31:52 -0400

    Added bubble chart

--- db/airtable.js
+++ db/airtable.js
@@ -1,52 +1,55 @@
+var low = require('lowdb');
+var _ = require('lodash');
 var Airtable = require('airtable');
+
 Airtable.configure({
-    endpointUrl: 'https://api.airtable.com',
-    apiKey: ''
+  endpointUrl: 'https://api.airtable.com',
+  apiKey: ''
 });
 var base = Airtable.base('appl4sMLlQp13Je1K');
+var db = low('tmp/db2.json');
 var count = 1;
+var transactions;
 
 base('Transactions').select({
     // Selecting the first 3 records in Main View:
     // maxRecords: 3,
     view: "Main View"
-}).eachPage(function page(records, fetchNextPage) {
+  }).eachPage(function page(records, fetchNextPage) {
 
     // This function (`page`) will get called for each page of records.
 
-    records.forEach(function(record) {
-        console.log('Retrieved ', count, ': ', record.get('Description'), record.get('Bucket'), record.get('Amount'));
-        count++;
-    });
+    // records.forEach(function(record) {
+    //     console.log('Retrieved ', count, ': ', record.get('Description'), record.get('Bucket'), record.get('Amount'));
+    //     count++;
+    // });
+
+transactions = records.map(record => {
+  console.log('Retrieved ', count, ': ', record.get('Description'), record.get('Bucket'), record.get('Amount'));
+  count++;
+  return {
+    "amount": Number(record.get('Amount')),
+    "date": record.get('Date'),
+    "year-month": _.join([_.split(record.get('Date'), '/')[2],_.split(record.get('Date'), '/')[0]],'-'),
+    "name": record.get('Description'),
+    "originalName": record.get('Original Description'),
+    "category": record.get('Category'),
+    "bucket": record.get('Bucket'),
+    "labels": record.get('Labels'),
+    "notes": record.get('Notes')
+  };
+});
+
 
     // To fetch the next page of records, call `fetchNextPage`.
     // If there are more records, `page` will get called again.
     // If there are no more records, `done` will get called.
     fetchNextPage();
 
-}, function done(error) {
+  }, function done(error) {
     if (error) {
-        console.log(error);
+      console.log(error);
+    } else {
+      db.defaults({ transactions: transactions }).value();
     }
-});
-
-    {
-      "_account": "TBD",
-      "account": "Regular Checking",
-      "_id": "f13ad567-544f-48db-886e-b69d13e155c5",
-      "amount": 284.05,
-      "date": "8/19/2016",
-      "year-month": "2016-8",
-      "name": "Transfer to 8572",
-      "originalName": "ACH HOLD CITI AUTOPAY PAYMENT ON 08/19",
-      "pending": false,
-      "type": {
-        "primary": "TBD",
-        "mint": "debit"
-      },
-      "category": [
-        "Credit Card Payment"
-      ],
-      "labels": [],
-      "notes": ""
-    },
+  });
